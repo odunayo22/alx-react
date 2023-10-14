5dd16addec37495ad2e6bdef51baec1efea2e9ea
odunayo22 <shakhordun@gmail.com>
2023-10-14 20:29:04 +0100

    react-intro

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+function App() {
+    return(
+        <div>
+            <Header className="App-header">
+                <img src=""/>
+                <h1>School dashboard</h1>
+            </Header>
+            <body className="App-body">
+                <p>Login to access the full dashboard</p>
+            </body>
+            <footer>
+
+            </footer>
+        </div>
+    )
+
+}
